139f0a4396e57ce9769e1ba90ff4399619592bdf
Carlos R. Rendon <carlos.ramirezrn@outlook.com>
2020-11-17 17:21:27 -0600

    Primer avance 17/11/20

--- fsm_gen.py
+++ fsm_gen.py
@@ -8,7 +8,6 @@ class FsmGenerator():
             self.data = json.load(json_file)
 
     def verify(self):
-
         key_words = [["format" , "data"] , ["actual_state", "next_state" , "inputs" , "outputs"]]
 
         flag_l1 = 0
@@ -17,22 +16,25 @@ class FsmGenerator():
         for key in self.data.keys():
             if key in key_words[0]:
                 flag_l1 += 1
-        print(flag_l1)
+
 
         if flag_l1 == len(self.data):
             for file, element in enumerate (self.data["data"]):
-                for key in self.data["data"][file].key():
-                    print(key)
+                for key in self.data["data"][file].keys():
                     if key in key_words[1]:
                         flag_l2 += 1
 
-        print(flag_l2)
 
-        if flag_l2 == len(self.data["data"]):
+        if flag_l2 % len(self.data["data"]) == 0:
             print("Verified file")
+            return True
 
         else:
             print("Warning!!!! Wrong format")
+            return False
+
+    #def state_treatment(self):
+
 
 
 if __name__ == "__main__":
--- template.py
+++ template.py
@@ -57,7 +57,7 @@ dict = {
     [
         {
             "actual_state" :"s0",
-            "next_state" :"S1",
+            "next_state" :"s1",
             "inputs" : [["A",0b1],["B",0b1]],
             "outputs" : [["x",0b01] , ["y" , 0b00]]
         },
