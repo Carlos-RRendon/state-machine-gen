251d766851043b034df0cbf9086105e3736897d0
Carlos R. Rendon <carlos.ramirezrn@outlook.com>
2020-11-17 16:17:48 -0600

    Agregar el JSON

--- template.py
+++ template.py
@@ -7,12 +7,13 @@ options = ["bin" , "hex", "dec"]
 #  State |  Next_State | Inputs | Outputs |
 #--------+-------------+--------+---------+
 
-dict = {
+
+'''dict = {
 "format" : options[0] ,
 "data":
     [
         {
-            "actual_state" :"S0",
+            "actual_state" :"s0",
             "next_state" :"S2",
             "inputs" : [["A",0b00001111],["B",15],["C",0xf]],
             "outputs" : [["x",0b10010100] , ["y" , 20], ["z", 0xff]]
@@ -47,17 +48,42 @@ data2 = {
             ]
     }
 
-
 }
+'''
 
+dict = {
+"format" : options[0],
+"data":
+    [
+        {
+            "actual_state" :"s0",
+            "next_state" :"S1",
+            "inputs" : [["A",0b1],["B",0b1]],
+            "outputs" : [["x",0b01] , ["y" , 0b00]]
+        },
 
+        {
+            "actual_state": "s1",
+            "next_state": "s2",
+            "inputs": [["A", 0b0], ["B", 0b1]],
+            "outputs": [["x", 0b00], ["y", 0b1]]
+        },
+{
+            "actual_state": "s2",
+            "next_state": "s3",
+            "inputs": [["A", 0b1], ["B", 0b0]],
+            "outputs": [["x", 0b11], ["y", 0b1]]
+        }
+    ]
+}
+if __name__ == "__main__" :
+    
 
+    json_data = json.dumps(dict , indent = 1)
 
-json_data = json.dumps(dict , indent = 1)
+    print(json_data)
 
-print(json_data)
-
-jsonFile = 'data.json'
-# Open a json file for writing json data
-with open(jsonFile, 'w') as fileHandler1:
-  json.dump(dict, fileHandler1, indent = 2)
+    jsonFile = 'data.json'
+    # Open a json file for writing json data
+    with open(jsonFile, 'w') as fileHandler1:
+      json.dump(dict, fileHandler1, indent = 2)
