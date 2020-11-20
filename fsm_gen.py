ebc70ae2d8cd71bf3e20adc5565ea52154e70bca
Carlos R. Rendon <carlangass002@hotmail.com>
2020-11-20 17:25:47 -0600

    Poniendo comentarios a python

--- fsm_gen.py
+++ fsm_gen.py
@@ -10,10 +10,10 @@ class FsmGenerator():
 # Reading of the manually filled .JSON file
 # -------------------------------------------------------------------------------------------    
 
-    def __init__(self, file):
+    def __init__(self, file):    #Initialize the class
 
         with open(file) as json_file:
-            self.data = json.load(json_file)
+            self.data = json.load(json_file)  #Creating an attribute data that will contain all the JSON info
 
         self.verify()
 
@@ -22,12 +22,12 @@ class FsmGenerator():
 # -------------------------------------------------------------------------------------------
 
     def verify(self):
-        key_words = [["format", "data"], ["actual_state", "next_state", "inputs", "outputs"]]
+        key_words = [["format", "data"], ["actual_state", "next_state", "inputs", "outputs"]] #Keywords to check the information of the user 
 
-        flag_l1 = 0
+        flag_l1 = 0     #Flags to check the JSON data keywords 
         flag_l2 = 0
 
-        for key in self.data.keys():
+        for key in self.data.keys():   
             if key in key_words[0]:
                 flag_l1 += 1
 
@@ -46,7 +46,7 @@ class FsmGenerator():
 
 
 
-        if flag_l2 % len(self.data["data"]) == 0:
+        if flag_l2 % len(self.data["data"]) == 0:   #Checking the lenfg of the flags to verify the JSON contains all the keywords
             print("Verified file")
             return True
 
@@ -64,43 +64,43 @@ class FsmGenerator():
 
         states = []
 
-        for state in self.data["data"]:
-            states.append(state["actual_state"])
+        for state in self.data["data"]:        
+            states.append(state["actual_state"])     #Check the total non repeated states
 
         #states = sorted(states)
-        states = collections.Counter(states)
-        return states
+        states = collections.Counter(states)  #Making the states array into a dictionary
+        return states                         #Returning the dictoinary with the name of the states and the number of repetitions
 
 # -------------------------------------------------------------------------------------------
 # FSM Ports width identification 
 # ------------------------------------------------------------------------------------------- 
 
-    def find_bus(self,key):
+    def find_bus(self,key):  #This function returns the bis width depending the max value of the inputs/outputs
 
         bus_dict = {}
 
-        for line in self.data["data"]:
+        for line in self.data["data"]:              #Iterator into the subsections inputs/outputs of data
 
-            for input in line[key]:
+            for input in line[key]:                    
 
                 if input[0] in bus_dict.keys():
 
                     bus_dict[input[0]].append(input[1])
                 else:
-                    bus_dict[input[0]] = [input[1]]
+                    bus_dict[input[0]] = [input[1]]       #Bus_dict will contain all the values of inputs/outputs
 
         for key in bus_dict.keys():
 
-            max_el = max(bus_dict[key])
+            max_el = max(bus_dict[key])                     #This function calculates the biggest value of the bus_dict array
 
             try:
-                max_el = math.floor(math.log(max_el)/math.log(2))+1
-                bus_dict[key] = max_el
+                max_el = math.floor(math.log(max_el)/math.log(2))+1 #Calculate the number of bits that will be needed to represent the value
+                bus_dict[key] = max_el                             
             except ValueError:
                 bus_dict[key] = -1
 
 
-        return bus_dict
+        return bus_dict                    #Return the dictionary with all the name of the inputs/outputs with its biggest value
 
 
 # -------------------------------------------------------------------------------------------
@@ -109,55 +109,58 @@ class FsmGenerator():
 
     def fsm_creator(self):
 
-        import os
+        import os   
 
-        states = self.state_treatment()
-        inputs_bus = self.find_bus("inputs")
-        outputs_bus = self.find_bus("outputs")
+        states = self.state_treatment()         #Calling the method that will return the name of the states and its number of repetitions 
+        inputs_bus = self.find_bus("inputs")    #Calling the method that will return the name of the all inputs with the bus
+        outputs_bus = self.find_bus("outputs")  #Calling the method that will return the name of the all outputs with the bus
 
-        name = "FSM_P.sv"
-
+        name = "FSM_P.sv"                       #Generating the name of the new file 
+        
+        #Adding comments for the template 
+        
         text_out = "//=============================================================================\n"
         text_out += "// FSM Verilog design\n"
         text_out += "//=============================================================================\n\n\n"
 
-
+        #Adding comments to the template
 
         text_out += "//-----------------------------------------------------------------------------\n"
         text_out += "// Module name and ports declaration\n" 
         text_out += "//-----------------------------------------------------------------------------\n\n"   
+        
+        
+        
+        text_out += "module state_machine (\n"    #Adding the name of the module in the template
 
-        text_out += "module state_machine (\n"
+        text_out += "input clk, rst,\n"           #Adding the clock and the reset inputs 
 
-        text_out += "input clk, rst,\n"
-
-        for key, value in inputs_bus.items():
+        for key, value in inputs_bus.items():     #For to add all the inputs with it's bus length
             if value == 1 :
-                text_out += f'input {key},\n'
+                text_out += f'input {key},\n'     #If to check if the bus with is 0 or 1 to add only the name
             else:
-                text_out += f'input [{value-1}:0] {key},\n'
+                text_out += f'input [{value-1}:0] {key},\n'   #If it has a bus length bigger than 1 then put the bus lenght 
 
 
-        for key, value in outputs_bus.items():
+        for key, value in outputs_bus.items():    #For to add all the outputs with it's bus length
 
-            if value == 1 :
+            if value == 1 :                        #If to check if the bus with is 0 or 1 to add only the name
                 text_out += f'output reg {key}'
-            else:
+            else:                                             #If it has a bus length bigger than 1 then put the bus lenght 
                text_out += f'output reg [{value-1}:0] {key}'
 
-
-            if key != (list(outputs_bus)[-1]):
+            if key != (list(outputs_bus)[-1]):          #Conditional to catch the last element of the outputs list to avoid the ,
                 text_out += ",\n"
 
 
-        text_out += ");\n\n"
+        text_out += ");\n\n"              #Closing the input-outputs declaration in the module
 
-
+        #Adding more coments to separate the sections
         text_out += "//-----------------------------------------------------------------------------\n"
         text_out += "// FSM states declaration\n" 
         text_out += "//-----------------------------------------------------------------------------\n" 
         
-        bits = len(states)
+        bits = len(states)                     #
         bits = math.ceil(math.log2(bits))
         first_state = None
 
